01f09c08fe3986b41b0232d4fc4c68b8268867f1
fujikawa38 <tacchi.juc.08lastwin@gmail.com>
2025-06-08 22:21:13 +0900

    login画面を作成。

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
 				.formLogin(form -> form
 						.loginPage("/login").defaultSuccessUrl("/", true).permitAll())
 				.logout(logout -> logout
-						.logoutSuccessUrl("/").permitAll());
+						.logoutSuccessUrl("/login?logout").permitAll());
 
 		return http.build();
 	}
